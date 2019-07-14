3bf45585dc4546eac2208d385c8a88d06af145e6
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2019-07-14 14:39:06 -0300

    fix(scripts): fix publish-package scripts

--- scripts/publish-package.sh
+++ scripts/publish-package.sh
@@ -1,8 +1,7 @@
 PROJECT_PATH="src/IdentityServerCli.Console"
-IS4_VERSION=$(git describe --abbrev=0)
 
 echo "Stage 1: Packing tool"
-dotnet pack $PROJECT_PATH
+export IS4_VERSION=$(git describe --abbrev=0) && dotnet pack $PROJECT_PATH
 
 echo "Stage 2: Publishing tool"
 dotnet nuget push $PROJECT_PATH/nupkg/IdentityServerCli.$IS4_VERSION.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
