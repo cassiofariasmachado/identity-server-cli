fe99f66160fab6ced3c81ccf890a3fa85fd4720c
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2019-07-14 13:50:27 -0300

    chore(): add source parameter to dotnet nuget push

--- scripts/publish-package.sh
+++ scripts/publish-package.sh
@@ -5,4 +5,4 @@ echo "Stage 2: Packing tool"
 dotnet pack $PROJECT_PATH
 
 echo "Stage 3: Publishing tool"
-dotnet nuget push $PROJECT_PATH/nupkg/IdentityServerCli.$IS4_VERSION.nupkg -k $NUGET_API_KEY
+dotnet nuget push $PROJECT_PATH/nupkg/IdentityServerCli.$IS4_VERSION.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
