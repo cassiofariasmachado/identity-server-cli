24fe9b83ca9fe1e461fa88ebedbfa6dc1511b830
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2019-07-14 14:03:32 -0300

    fix(): fix travis build

--- scripts/publish-package.sh
+++ scripts/publish-package.sh
@@ -1,8 +1,8 @@
 PROJECT_PATH="src/IdentityServerCli.Console"
-IS4_VERSION=$(git describe)
+IS4_VERSION=$(git describe --abbrev=0)
 
-echo "Stage 2: Packing tool"
+echo "Stage 1: Packing tool"
 dotnet pack $PROJECT_PATH
 
-echo "Stage 3: Publishing tool"
+echo "Stage 2: Publishing tool"
 dotnet nuget push $PROJECT_PATH/nupkg/IdentityServerCli.$IS4_VERSION.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
